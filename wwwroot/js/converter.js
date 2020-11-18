ccee457e86ae9a1536187148c9b8acb81b7e49b2
Mark M <otteromkram@gmail.com>
2020-11-18 13:42:21 -0600

    restructure and annotations added.

--- wwwroot/js/converter.js
+++ wwwroot/js/converter.js
@@ -8,59 +8,71 @@ Sample ID:
 */
 
 
+// Sample Salesforce ID
+const sfid15 = "9062I000000ITnk"; // 90630000000gtfAAAQ
 
-
-
-/** 
- * @author Mark Moretto
- * @returns {boolean} true if single character is uppercase; false otherwise.
- * @description String property to evaluate whether a single character is uppercase.
- * @example <caption>Determine whether 'A' is uppercase.</caption>
- * // returns true
- * "A".isUpper
-*/
-
-Object.defineProperty(String.prototype, "isUpper", {
-    get() {
-        return this.length === 1 ? (this.toString() === this.toString().toUpperCase() && isNaN(this.toString() * 1)) ? true : false : "Property requires single character.";
-    }
-});
 
 
 // Set alphanumeric list
 let codeList = [];
-let j = 65;
-let k = 0;
-const alphaStopIdx = 91;
-while (j < 97) { 
-    j < alphaStopIdx ? codeList.push(String.fromCharCode(j)) : codeList.push(`${j - alphaStopIdx}`);
-    j++;
-    k++;
-}
 
-
-var sfid15 = "9062I000000ITnk"; // 90630000000gtfAAAQ
-
-
-function ConvertID(SfidObj) {
-    let i = 0;
-    const stepsize = 5;
-    let letters = [];
-
-    while (i < SfidObj.length) {
-        let chunk = SfidObj.slice(i, i + stepsize);
-        let idx = 0;
-        let tot = 0;
-        chunk.split("").forEach( el => {
-            tot += el.isUpper ? Math.pow(2, idx) : 0;
-            idx++;
-        });
-        letters.push(codeList[tot]);
-        i += stepsize;
+function setCodeList() {
+    let j = 65;
+    let k = 0;
+    const alphaStopIdx = 91;
+    while (j < 97) { 
+        j < alphaStopIdx ? codeList.push(String.fromCharCode(j)) : codeList.push(`${j - alphaStopIdx}`);
+        j++;
+        k++;
     }
-
-    let newID = sfid15 + letters.join("");
-    console.assert(newID.length === 18, `Error: NewId character count mismatch.`);
-    return newID;
 }
 
+/**
+ * Convert 15-character ID to 18-character ID.
+ * @param {String} SfidObj - Salesforce ID.
+*/
+function IdConverter(SfidObj) {
+    const stepsize = 5;
+    if (SfidObj.length === 15) {
+        let i = 0;
+
+        let letters = "";
+
+        while (i < SfidObj.length) {
+            // Chunk into three sections of five characters
+            let chunk = SfidObj.slice(i, i + stepsize);
+
+            // Set index and cumulative total variables to zero.
+            let idx = 0;
+            let tot = 0;
+
+            // Split substring and determine if each character is uppercase.
+            // If so, raise 2 to the power of the index value (range -> 0 to 4).
+            chunk.split("").forEach( el => {
+                tot += el.isUpper ? Math.pow(2, idx) : 0;
+                idx++;
+            });
+            // Append character result to worker variable.
+            letters += codeList[tot];
+
+            // Increment starting index by step size value.
+            i += stepsize;
+        }
+
+        // Append new character set to Salesforce ID object.
+        SfidObj += letters;
+
+        // Assert that we now have 18 characters for our ID.
+        console.assert(SfidObj.length === 18, `Error: NewId character count mismatch.`);
+        
+    }
+    return SfidObj;
+}
+
+
+/**
+ * Set code list when DOM content completes loading.
+*/
+window.addEventListener("DOMContentLoaded", () => {
+    setCodeList();
+});
